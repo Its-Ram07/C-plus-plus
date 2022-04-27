1c737ffe79b9c2663de4a140161aa2277d390d8f
Ram <ramdas.sahu.cs.20@ggits.net>
2022-04-28 00:08:11 +0530

    Update 1_Writing Hello World program.cpp

--- 1_Writing Hello World program.cpp
+++ 1_Writing Hello World program.cpp
@@ -1,6 +1,7 @@
-#include<iostream>				//Header file (for basic its just a part of c++ syntax or a must line in a c++ program )
+#include<iostream>			//Header file (for basic its just a part of c++ syntax or a must line in a c++ program )
 using namesapce std;			//its a common line for printing out any statement overall the program
 
-int main(){						//Every program have a (main) function any other function than main function is also required to be called in the main fnction to be    									//executed 
+int main(){				//Every program have a (main) function any other function than main function 
+	                                ///is also required to be called in the main fnction to be executed 
 	cout<<"Hello World";		// cout :- syntax used for printing out any line or variable
 	}
