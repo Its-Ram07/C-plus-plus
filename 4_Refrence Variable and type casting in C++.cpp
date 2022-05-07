a43ee4261966e9a58c13833c440993d59b9d3c42
Ram <ramdas.sahu.cs.20@ggits.net>
2022-05-07 23:18:23 +0530

    Create 4_Refrence Variable and type casting in C++.cpp

---
+++ 4_Refrence Variable and type casting in C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int a,b,c;
+	cout << "enter the value of num1 :\n";   		
+	cin >> a;					 	 
+	cout << "enter the value of num2 :\n"; 	  		 
+	cin >> b;				 	       
+  a+b=c;
+	cout<<"the sum is "<<c;	
+
+	return 0;
+}
